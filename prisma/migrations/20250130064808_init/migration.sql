9ed9675287b4b58795a911e0e7b3bc97128c20be
Shimano Koya <qiaoyedaoye@gmail.com>
2025-01-30 15:54:07 +0900

    Change database to postgresql

--- prisma/migrations/20250130064808_init/migration.sql
+++ prisma/migrations/20250130064808_init/migration.sql
@@ -1,9 +1,11 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "password" TEXT NOT NULL
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex

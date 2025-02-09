2ed95457940300b5cd5186faab4927ee5a7d621f
Shimano Koya <qiaoyedaoye@gmail.com>
2025-02-09 20:49:44 +0900

    Edit file name

--- src/components/MarkdownContent.tsx
+++ src/components/MarkdownContent.tsx
@@ -4,6 +4,7 @@ import { htmlToPdf } from "@/actions/html_to_pdf";
 import Markdown from "@/components/Markdown";
 import { getHtml } from "@/utils";
 import { useEffect, useRef, useState } from "react";
+import { FaDownload } from "react-icons/fa";
 
 type MarkdownContentProps = {
   markdown: string;
@@ -12,11 +13,14 @@ type MarkdownContentProps = {
 
 export default function MarkdownContent({
   markdown: initialMarkdown,
-  fileName,
+  fileName: initialFileName,
 }: MarkdownContentProps) {
   const [markdown, setMarkdown] = useState(initialMarkdown);
+  const [fileName, setFileName] = useState(initialFileName);
   const markdownRef = useRef<HTMLDivElement>(null);
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const isFileNameValid = !/\.(md|pdf|html)$/i.test(fileName);
 
   useEffect(() => {
     const debounceTimeout = setTimeout(() => {
@@ -93,25 +97,48 @@ export default function MarkdownContent({
 
   return (
     <>
-      <div className="flex justify-end gap-2">
-        <button
-          onClick={() => void downloadPdf()}
-          className="inline-flex justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
-        >
-          PDFをダウンロード
-        </button>
-        <button
-          onClick={downloadMarkdown}
-          className="inline-flex justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
-        >
-          Markdownをダウンロード
-        </button>
-        <button
-          onClick={() => downloadHtml()}
-          className="inline-flex justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
-        >
-          HTMLをダウンロード
-        </button>
+      <div className="flex items-start justify-between gap-2">
+        <div className="flex flex-1 flex-col gap-2">
+          <label htmlFor="fileName">ファイル名</label>
+          <input
+            id="fileName"
+            type="text"
+            value={fileName}
+            onChange={(e) => setFileName(e.target.value)}
+            className="max-w-md rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+          />
+          <p
+            className={
+              "text-xs " + (isFileNameValid ? "text-gray-500" : "text-red-500")
+            }
+          >
+            拡張子を除く
+          </p>
+        </div>
+        <div className="flex items-center gap-2">
+          <FaDownload className="mr-1 text-green-400" />
+          <button
+            onClick={() => void downloadPdf()}
+            className="inline-flex justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 disabled:bg-gray-400 disabled:opacity-50"
+            disabled={!isFileNameValid}
+          >
+            PDF
+          </button>
+          <button
+            onClick={downloadMarkdown}
+            className="inline-flex justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 disabled:bg-gray-400 disabled:opacity-50"
+            disabled={!isFileNameValid}
+          >
+            Markdown
+          </button>
+          <button
+            onClick={() => downloadHtml()}
+            className="inline-flex justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 disabled:bg-gray-400 disabled:opacity-50"
+            disabled={!isFileNameValid}
+          >
+            HTML
+          </button>
+        </div>
       </div>
       <div className="flex gap-4">
         <textarea
